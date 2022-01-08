7c8b480fc0a30921a7d4cd4723bb7adfa694195c
Adrian Juszczak <adrian@juszczak.io>
2022-01-08 14:50:28 +0100

    Zadanie 10
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 function delayedHello(text) {
   console.log(text);
 }
@@ -7,9 +5,8 @@ function delayedHello(text) {
 // delayedHello();
 // setTimeout(delayedHello, 5000, 'Hello!');
 
-var intervalId = setInterval(delayedHello, 1000, 'Hello Interval!');
+var intervalId = setInterval(delayedHello, 1000, "Hello Interval!");
 clearInterval(intervalId);
-
 
 /**
  * Zadanie 1
@@ -20,8 +17,8 @@ clearInterval(intervalId);
 function delayMessage(text) {
   var timeout = Math.round(Math.random() * 10) * 1000;
 
-  setTimeout(function() {
-    console.log(text, '(', timeout, ')');
+  setTimeout(function () {
+    console.log(text, "(", timeout, ")");
   }, timeout);
 }
 
@@ -31,18 +28,19 @@ function delayMessage(text) {
  */
 
 function setRandomTitle() {
-  setInterval(function() {
+  setInterval(function () {
     var title = Math.random().toString(36);
     document.title = title;
   }, 1000);
 }
 
-
 function newWindow() {
-
-  var newWindowObject = window.open('/child.html', 'nazwaOkna1', 'status,width=300,height=300');
+  var newWindowObject = window.open(
+    "/child.html",
+    "nazwaOkna1",
+    "status,width=300,height=300"
+  );
   console.log(newWindowObject);
-
 }
 
 /*
@@ -52,28 +50,28 @@ function newWindow() {
 */
 
 function openWindowWithText(text) {
-  var win = window.open('/child.html', 'child', 'status,width=300,height=300');
-  win.document.write('<h1>' + text + '</h1>');
+  var win = window.open("/child.html", "child", "status,width=300,height=300");
+  win.document.write("<h1>" + text + "</h1>");
   win.document.close();
 }
 
-var p1 = document.getElementById('p1'); // 1 element o podanym id
+var p1 = document.getElementById("p1"); // 1 element o podanym id
 
-var headings = document.getElementsByClassName('section-heading'); // kolekcja elementów z daną klasą
+var headings = document.getElementsByClassName("section-heading"); // kolekcja elementów z daną klasą
 
-var passwords = document.getElementsByName('password'); // lista elementów o podanej nazwie
+var passwords = document.getElementsByName("password"); // lista elementów o podanej nazwie
 
-var links = document.getElementsByTagName('a'); // kolekcja elementów z tagiem <a>
+var links = document.getElementsByTagName("a"); // kolekcja elementów z tagiem <a>
 
-var firstHeading = document.querySelector('.section-heading'); // pierwszy element z klasą section-heading
+var firstHeading = document.querySelector(".section-heading"); // pierwszy element z klasą section-heading
 
-var queriedHeadings = document.querySelectorAll('.section-heading'); // lista elementów z klasą section-heading
+var queriedHeadings = document.querySelectorAll(".section-heading"); // lista elementów z klasą section-heading
 
 // inner text
-var lorem = document.getElementById('p1');
-lorem.innerText = 'Nowa wartość tekstowa';
+var lorem = document.getElementById("p1");
+lorem.innerText = "Nowa wartość tekstowa";
 
-p1.style.backgroundColor = '#f0f';
+p1.style.backgroundColor = "#f0f";
 
 /**
  * Zadanie 4
@@ -81,23 +79,21 @@ Napisz funkcję ustawiającą przekazany jako argument tekst dla wskazanego elem
 */
 
 function setElementText(text) {
-  var element = document.getElementById('p1');
+  var element = document.getElementById("p1");
   element.innerText = text;
 }
 
-setElementText('Hello element');
-
+setElementText("Hello element");
 
 // STATYCZNA lista elementów: NodeList
-var nodeList = document.querySelectorAll('.section-heading');
+var nodeList = document.querySelectorAll(".section-heading");
 
 // DYNAMICZNA kolekcja elementów: HTMLCollection
-var links = document.getElementsByTagName('a');
+var links = document.getElementsByTagName("a");
 
-document.getElementById('scroll-container').scrollHeight
+document.getElementById("scroll-container").scrollHeight;
 
-document.getElementById('scroll-container').scrollTop
-
+document.getElementById("scroll-container").scrollTop;
 
 /**
  * Zadanie 5
@@ -115,14 +111,14 @@ function countElements(element) {
   return count;
 }
 
-var element1 = document.createElement('div');
-element1.id = 'element1';
-element1.classList.add('box');
-element1.innerText = 'Dynamicznie stworzony element1'
+var element1 = document.createElement("div");
+element1.id = "element1";
+element1.classList.add("box");
+element1.innerText = "Dynamicznie stworzony element1";
 
-var element2 = document.createElement('span');
-element2.id = 'span1';
-element2.innerText = 'zawartość elementu span';
+var element2 = document.createElement("span");
+element2.id = "span1";
+element2.innerText = "zawartość elementu span";
 
 /**
  * Zadanie 6
@@ -131,14 +127,14 @@ Napisz skrypt który wygeneruje tabelę table posiadającą
 */
 
 function generateTable() {
-  var table = document.createElement('table');
+  var table = document.createElement("table");
   var counter = 0;
 
   for (var i = 0; i < 100; i++) {
-    var tr = document.createElement('tr');
+    var tr = document.createElement("tr");
 
     for (var j = 0; j < 10; j++) {
-      var td = document.createElement('td');
+      var td = document.createElement("td");
       td.innerText = counter++;
       tr.append(td);
     }
@@ -146,24 +142,24 @@ function generateTable() {
     table.append(tr);
   }
 
-  var container = document.getElementById('table-container');
+  var container = document.getElementById("table-container");
   container.append(table);
 }
 
 generateTable();
 
-var span3 = document.createElement('span');
-span3.id = 'span3';
-span3.innerText = 'span 3 content'
-span3.style.backgroundColor = '#ff0';
+var span3 = document.createElement("span");
+span3.id = "span3";
+span3.innerText = "span 3 content";
+span3.style.backgroundColor = "#ff0";
 
-document.getElementById('parent1').append(span3) // dodanie spana do parent1
-document.getElementById('parent2').append(span3) // dodanie spana do parent2 (+ usunięcie z parent1)
+document.getElementById("parent1").append(span3); // dodanie spana do parent1
+document.getElementById("parent2").append(span3); // dodanie spana do parent2 (+ usunięcie z parent1)
 
 // var span4 = span3.cloneNode(); // klon elementu span3 bez zawartości
 var span4 = span3.cloneNode(true); // klon elementu span3 z zawartością
 
-document.getElementById('parent1').append(span4) // dodanie spana do parent1
+document.getElementById("parent1").append(span4); // dodanie spana do parent1
 
 span3.remove(); // kasowanie elementu z drzewa
 
@@ -179,26 +175,26 @@ window.addEventListener('load', function(event) {
 });
 */
 
-var clickListener = function(event) {
-  console.log('click', event);
-}
+var clickListener = function (event) {
+  console.log("click", event);
+};
 
 // document.addEventListener('click', clickListener);
 
-var parent2 = document.getElementById('parent2');
-parent2.addEventListener('click', function(event) {
+var parent2 = document.getElementById("parent2");
+parent2.addEventListener("click", function (event) {
   console.log(event.type);
   console.log(event.target);
   console.log(event.currentTarget);
 });
 
-var button1 = document.getElementById('button1');
-button1.addEventListener('click', function() {
-  console.log('button has been clicked!');
+var button1 = document.getElementById("button1");
+button1.addEventListener("click", function () {
+  console.log("button has been clicked!");
 });
 
-var link1 = document.getElementById('child-link');
-link1.addEventListener('click', function(event) {
+var link1 = document.getElementById("child-link");
+link1.addEventListener("click", function (event) {
   event.preventDefault();
 });
 
@@ -217,10 +213,11 @@ document.addEventListener('click', function(event) {
 });
 */
 
-document.getElementById('button2').addEventListener('mousemove', function(event) {
-  console.log(event.type, event.clientX, event.clientY);
-});
-
+document
+  .getElementById("button2")
+  .addEventListener("mousemove", function (event) {
+    console.log(event.type, event.clientX, event.clientY);
+  });
 
 /*
  * Zadanie 8
@@ -229,38 +226,39 @@ podwójnymkliknięciem myszki a następnie na przeniesienie go w
 miejsce wystąpienia kolejnego pojedynczego kliknięcia.
 */
 
-var movable = document.getElementById('movable');
+var movable = document.getElementById("movable");
 
-movable.addEventListener('dblclick', function(event) {
-  event.target.style.border = '2px dotted #110';
+movable.addEventListener("dblclick", function (event) {
+  event.target.style.border = "2px dotted #110";
 
-  document.addEventListener('click', function(secondEvent) {
-    event.target.style.position = 'fixed';
-    event.target.style.border = 'none';
-    event.target.style.top = secondEvent.clientY + 'px';
-    event.target.style.left = secondEvent.clientX + 'px';
-  }, { once: true });
-
+  document.addEventListener(
+    "click",
+    function (secondEvent) {
+      event.target.style.position = "fixed";
+      event.target.style.border = "none";
+      event.target.style.top = secondEvent.clientY + "px";
+      event.target.style.left = secondEvent.clientX + "px";
+    },
+    { once: true }
+  );
 });
 
-var tableContainer = document.getElementById('table-container');
+var tableContainer = document.getElementById("table-container");
 
-tableContainer.addEventListener('scroll', function(event) {
-  console.log('table scroll', event.target.scrollTop);
+tableContainer.addEventListener("scroll", function (event) {
+  console.log("table scroll", event.target.scrollTop);
 });
 
-var buttons = document.getElementsByClassName('button-test');
+var buttons = document.getElementsByClassName("button-test");
 
 for (var i = 0; i < buttons.length; i++) {
   var button = buttons[i];
 
-  button.addEventListener('click', function(event) {
-    event.target.innerText = 'clicked';
+  button.addEventListener("click", function (event) {
+    event.target.innerText = "clicked";
     console.log(event.target.id);
   });
-
 }
-
 
 // document.addEventListener('scroll', function(event) {
 //   console.log('document scroll', event.target.scrollTop);
@@ -271,12 +269,95 @@ for (var i = 0; i < buttons.length; i++) {
   Napisz skrypt pozwalający na wyświetlenie obok scrollowanego elementu wskaźnik postępu przewijania w procentach.
 */
 
-var scroller = document.getElementById('table-container');
-var progress = document.getElementById('progress');
+var scroller = document.getElementById("table-container");
+var progress = document.getElementById("progress");
 
-scroller.addEventListener('scroll', function(event) {
-  var percent = Math.round((event.target.scrollTop / (event.target.scrollHeight - event.target.clientHeight)) * 100);
+scroller.addEventListener("scroll", function (event) {
+  var percent = Math.round(
+    (event.target.scrollTop /
+      (event.target.scrollHeight - event.target.clientHeight)) *
+      100
+  );
 
-  progress.innerText = percent + ' %';
+  progress.innerText = percent + " %";
+});
+
+// document.addEventListener('keyup', function(event) {
+//   if (event.key === 'ArrowDown') {
+//     console.log('arrow down pressed');
+//   }
+// });
+
+// document.addEventListener('keydown', function(event) {
+//   console.log(event.type);
+// });
+
+// document.addEventListener('keypress', function(event) {
+//   console.log(event.type);
+// });
+
+/*
+ * Zadanie 10
+Napisz skrypt pozwalający przestawiać zaznaczony element strzałkami na klawiaturze.
+*/
+
+/*
+var movable = document.getElementById("movable");
+
+movable.addEventListener("click", function (event) {
+  event.target.style.border = "2px dotted #110";
+
+  document.addEventListener("keydown", function (secondEvent) {
+    secondEvent.preventDefault();
+    event.target.style.position = "fixed";
+    console.log(secondEvent.clienty);
+
+    if (secondEvent.key === "ArrowDown") {
+      event.target.style.top = (event.target.style.top - 10) + "px";
+    } else if (secondEvent.key === "ArrowUp") {
+      event.target.style.top = (event.target.style.top + 10) + "px";
+    } else if (secondEvent.key === "ArrowRight") {
+      event.target.style.left = secondEvent.clientX + "px";
+    } else if (secondEvent.key === "ArrowLeft") {
+    }
+  });
+});
+*/
+
+var keyboardMovable = document.getElementById('movable2');
+
+keyboardMovable.addEventListener('dblclick', function(event) {
+  event.target.style.border = '2px dotted #110';
+
+  document.addEventListener('keydown', function(keyboardEvent) {
+    keyboardEvent.preventDefault();
+    console.log(keyboardEvent.key);
+
+    switch (keyboardEvent.key) {
+      case 'ArrowLeft': {
+        var left = parseInt(event.target.style.left) - 10;
+        console.log(left);
+        event.target.style.left = left + 'px';
+        break;
+      }
+      case 'ArrowRight': {
+        var left = parseInt(event.target.style.left) + 10;
+        console.log(left);
+        event.target.style.left = left + 'px';
+        break;
+      }
+      case 'ArrowUp': {
+        var top = parseInt(event.target.style.top) - 10;
+        event.target.style.top = top + 'px';
+        break;
+      }
+      case 'ArrowDown': {
+        var top = parseInt(event.target.style.top) + 10;
+        event.target.style.top = top + 'px';
+        break;
+      }
+    }
+
+  });
 
 });
