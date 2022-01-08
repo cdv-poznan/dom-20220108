b2a0d45713551d0edfbdc2c183e43f0bc83a30b9
Adrian Juszczak <adrian@juszczak.io>
2022-01-08 09:05:51 +0100

    Zadanie 1
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('Hello DOM!');
+
+
+function delayedHello(text) {
+  console.log(text);
+}
+
+// delayedHello();
+// setTimeout(delayedHello, 5000, 'Hello!');
+
+var intervalId = setInterval(delayedHello, 1000, 'Hello Interval!');
+clearInterval(intervalId);
+
+
+/**
+ * Zadanie 1
+   Napisz funkcję, która po wywołaniu wyświetli przekazany jako
+   argument komunikat po upływie losowego czasu (do kilku sekund).
+ */
+
+function delayMessage(text) {
+  var timeout = Math.round(Math.random() * 10) * 1000;
+
+  setTimeout(function() {
+    console.log(text, '(', timeout, ')');
+  }, timeout);
+}
